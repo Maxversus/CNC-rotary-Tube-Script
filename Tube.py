94b0a6c739e37e866d3c09340c6edb17cad4403d
Max <maxversus@yandex.ru>
2024-02-26 23:30:23 +0300

    Заменил ось Y на A

--- Tube.py
+++ Tube.py
@@ -1,30 +1,24 @@
-from gcodeparser import GcodeParser
-
+import re
 from Gcodecreate import GCode
-# открыть файл gcode и сохранить содержимое в переменной
-with open('Круг.tap', 'r') as f:
-    gcode = f.read()
-new_gcode = []
-# получить разобранные строки gcode
-parsed_gcode = GcodeParser(gcode, include_comments=True).lines
-for line in parsed_gcode:
-    # заменить команды G2 и G3
-    if line.command == ('G', 2) or line.command == ('G', 3):
-        print(f'Найдена команда {line.params["X"], line.params["Y"], line.params["I"], line.params["J"]} в позиции {(prev_x, prev_y)}')
-        arc_lines = (GCode.arc_to_lines((prev_x, prev_y), (line.params["X"], line.params["Y"]), line.params["I"], line.params["J"], clockwise=True, num_segments=20))
-        for l in arc_lines:
-            new_line = ('G1 X{} Y{}'.format(l[0], l[1]))
-            new_gcode.append(new_line)
-    else:
-        new_gcode.append(line.gcode_str)
-    
-        
-    if 'X' in line.params:
-        prev_x = line.params['X']
-    if 'Y' in line.params:
-        prev_y = line.params['Y']
+D = 40 # Pipe diameter
+filename = "my_new_gcode.gcode" # Add the name of the file you want to open. "NameFile.CNC"
 
-# сохранить новый gcode в файл
-with open('my_new_gcode.gcode', 'w') as f:
-    for line in new_gcode:
-        f.write(str(line) + '\n')
+divisor = (D*3.1415926535)/360
+
+with open(f"{filename}", "r+") as file:
+    content = file.read()
+
+def replace_y_values(code, divisor):
+    
+    def replacer(match):
+        original_value = float(match.group()[1:])
+        new_value = '{:.2f}'.format(original_value / divisor)
+        return f'A{new_value}'
+
+    return re.sub(r'Y-?\d+\.\d+', replacer, code)
+
+content = replace_y_values(content, divisor)
+
+print(content)
+with open(f"{filename}", "w") as file:
+    file.write(content)
---
+++ arc_to_lines.py
@@ -0,0 +1,29 @@
+from gcodeparser import GcodeParser
+
+from Gcodecreate import GCode
+# открыть файл gcode и сохранить содержимое в переменной
+with open('Круг.tap', 'r') as f:
+    gcode = f.read()
+new_gcode = []
+# получить разобранные строки gcode
+parsed_gcode = GcodeParser(gcode, include_comments=True).lines
+for line in parsed_gcode:
+    # заменить команды G2 и G3
+    if line.command == ('G', 2) or line.command == ('G', 3):
+        print(f'Найдена команда {line.params["X"], line.params["Y"], line.params["I"], line.params["J"]} в позиции {(prev_x, prev_y)}')
+        arc_lines = (GCode.arc_to_lines((prev_x, prev_y), (line.params["X"], line.params["Y"]), line.params["I"], line.params["J"], clockwise=True, num_segments=20))
+        for l in arc_lines:
+            new_line = ('G1 X{} Y{}'.format(l[0], l[1]))
+            new_gcode.append(new_line)
+    else:
+        new_gcode.append(line.gcode_str)
+
+    if 'X' in line.params:
+        prev_x = line.params['X']
+    if 'Y' in line.params:
+        prev_y = line.params['Y']
+
+# сохранить новый gcode в файл
+with open('my_new_gcode.gcode', 'w') as f:
+    for line in new_gcode:
+        f.write(str(line) + '\n')
